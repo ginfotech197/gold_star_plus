3627cb26d253704dbcc1f3a0b2d79639bba20685
mounitbhandari <tataibhandari@gmail.com>
2021-10-26 20:19:04 +0530

    add two-d-number-combinations table

---
+++ gold_star_plus_api/database/factories/TwoDNumberCombinationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TwoDNumberCombination;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TwoDNumberCombinationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TwoDNumberCombination::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
