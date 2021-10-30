a673ee277a5830048fabb8be27886bc85ac36102
sreeparnadas <sreeparnadas0675@gmail.com>
2021-10-30 17:54:18 +0530

    dar

--- gold_star_plus_api/database/migrations/2022_05_50_170205_create_result_masters_table.php
+++ gold_star_plus_api/database/migrations/2022_05_50_170205_create_result_masters_table.php
@@ -17,7 +17,7 @@ class CreateResultMastersTable extends Migration
             $table->id();
 
             $table->foreignId('draw_master_id')->references('id')->on('draw_masters')->onDelete('cascade');
-            $table->foreignId('two_digit_number_combination_id ')->references('id')->on('two_digit_number_combinations')->onDelete('cascade');
+            $table->foreignId('two_digit_number_combination_id')->references('id')->on('two_digit_number_combinations')->onDelete('cascade');
 
             $table->date('game_date');
 
