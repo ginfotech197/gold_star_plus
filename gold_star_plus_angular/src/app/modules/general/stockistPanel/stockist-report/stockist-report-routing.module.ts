ad4dfa99a58d754f19111bc5e89a794607c77f4f
priyamghosh1 <priyamghosh73@gmail.com>
2021-11-17 13:45:37 +0530

    stockiest report updated

--- gold_star_plus_angular/src/app/modules/general/stockistPanel/stockist-report/stockist-report-routing.module.ts
+++ gold_star_plus_angular/src/app/modules/general/stockistPanel/stockist-report/stockist-report-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {StockistReportComponent} from '../../../../pages/stockistPanel/stockist-report/stockist-report.component';
 
-const routes: Routes = [
-  // { path: '', canActivate : [AuthGuardStockistServiceService], component: StockistReportComponent }
-  { path: '', component: StockistReportComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- gold_star_plus_angular/src/app/modules/general/stockistPanel/stockist-report/stockist-report.module.ts
+++ gold_star_plus_angular/src/app/modules/general/stockistPanel/stockist-report/stockist-report.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MasterStockistRoutingModule} from '../../cpanel/master-stockist/master-stockist-routing.module';
+
+import { StockistReportRoutingModule } from './stockist-report-routing.module';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MaterialModule} from '../../../../core/material.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
@@ -10,25 +11,24 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSortModule} from '@angular/material/sort';
 import {MatSelectModule} from '@angular/material/select';
 import {MatIconModule} from '@angular/material/icon';
+import {NgbModalModule, NgbToastModule} from '@ng-bootstrap/ng-bootstrap';
+import {MDBRootModule} from 'angular-bootstrap-md';
+import {PopoverModule} from 'ngx-smart-popover';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {StockistReportComponent} from '../../../../pages/stockistPanel/stockist-report/stockist-report.component';
-import {AdminReportsRoutingModule} from "../../cpanel/admin-reports/admin-reports-routing.module";
-import {NgbModalModule, NgbToastModule} from "@ng-bootstrap/ng-bootstrap";
-import {MDBRootModule} from "angular-bootstrap-md";
-import {PopoverModule} from "ngx-smart-popover";
-import {MatBadgeModule} from "@angular/material/badge";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatInputModule} from "@angular/material/input";
-import {MatNativeDateModule, MatRippleModule} from "@angular/material/core";
-import {MatButtonModule} from "@angular/material/button";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import {MatFormFieldModule} from "@angular/material/form-field";
 
 
 @NgModule({
   // declarations: [],
   imports: [
     CommonModule,
-    AdminReportsRoutingModule,
+    StockistReportRoutingModule,
     MatTabsModule,
     MaterialModule,
     FlexLayoutModule,
