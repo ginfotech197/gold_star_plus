a10bda553412e8ec119c6093616d6bc58497b0b7
priyamghosh1 <priyamghosh73@gmail.com>
2021-11-17 15:15:13 +0530

    added in dev

--- gold_star_plus_api/routes/api.php
+++ gold_star_plus_api/routes/api.php
@@ -195,6 +195,9 @@ Route::group(array('prefix' => 'dev'), function() {
     Route::post('terminal/terminal_sale_reports', [TerminalReportController::class, 'terminal_sale_reports']);
     Route::post('terminal/barcodeReport',[TerminalReportController::class, 'barcode_wise_report_by_terminal']);
 
+    Route::post('stockist/customerSaleReports', [StockistController::class, 'customer_sale_reports']);
+    Route::post('stockist/barcodeReportByDate', [StockistController::class, 'barcode_wise_report_by_date']);
+
 
     Route::post('testPrize',[CPanelReportController::class, 'get_total_amount_by_barcode']);
 
