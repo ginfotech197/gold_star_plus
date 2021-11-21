269e0da388b3763194c18c8927bc7733d5207431
priyamghosh1 <priyamghosh73@gmail.com>
2021-11-21 13:50:12 +0530

    two tabs created

--- gold_star_plus_angular/src/app/modules/general/cpanel/manual-result/manual-result.module.ts
+++ gold_star_plus_angular/src/app/modules/general/cpanel/manual-result/manual-result.module.ts
@@ -14,6 +14,10 @@ import {MatBadgeModule} from '@angular/material/badge';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MaterialModule} from "../../../../core/material.module";
+import {MatTabsModule} from "@angular/material/tabs";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatSortModule} from "@angular/material/sort";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
@@ -31,8 +35,13 @@ import {MaterialModule} from "../../../../core/material.module";
     MatSelectModule,
     MatBadgeModule,
     MatButtonModule,
-    MatSlideToggleModule,
-    MaterialModule
+    MaterialModule,
+
+    MatTabsModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatSortModule,
+    MatIconModule
 
   ],
   exports: [
